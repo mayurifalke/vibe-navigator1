8d0aa3d18a407798baf23a6479a381540f40365a
mayurifalke <152616198+mayurifalke@users.noreply.github.com>
2025-07-13 07:05:49 +0530

    Create static_places.py

---
+++ Flask Backend/static_places.py
@@ -0,0 +1,184 @@
+static_places = {
+    "Delhi": {
+        "cafe": [
+            "Rose Cafe",
+            "Triveni Terrace Cafe",
+            "The Big Chill",
+            "Music & Mountains - Hillside Cafe",
+            "The Hudson Cafe",
+            "Cafe Lota",
+            "Perch Wine & Coffee Bar",
+            "Diggin Cafe",
+            "Blue Tokai Coffee Roasters, Saket",
+            "Social Hauz Khas"
+        ],
+        "park": [
+            "Lodhi Garden",
+            "Central Park",
+            "Deer Park",
+            "Nehru Park",
+            "Millennium Park",
+            "Indraprastha Park",
+            "Japanese Park",
+            "Sanjay Lake Park",
+            "Buddha Jayanti Park",
+            "Garden of Five Senses"
+        ]
+    },
+    "Mumbai": {
+        "cafe": [
+            "Leopold Cafe",
+            "Prithvi Cafe",
+            "Blue Tokai Coffee, Lower Parel",
+            "Kala Ghoda Cafe",
+            "Bombay Coffee House",
+            "Cafe Mondegar",
+            "Starbucks Fort",
+            "Coffee By Di Bella, Bandra",
+            "The Nutcracker, Bandra",
+            "The Pantry Cafe"
+        ],
+        "park": [
+            "Hanging Gardens",
+            "Sanjay Gandhi National Park",
+            "Priyadarshini Park",
+            "Jogger's Park",
+            "Five Gardens",
+            "Veermata Jijabai Udyan (Byculla Zoo)",
+            "Shivaji Park",
+            "Mahim Nature Park",
+            "Powai Lake Park",
+            "Bandra Fort Garden"
+        ]
+    },
+    "Bangalore": {
+        "cafe": [
+            "Third Wave Coffee Roasters",
+            "Matteo Coffea",
+            "DYU Art Cafe",
+            "Cafe Noir UB City",
+            "The Hole in the Wall Cafe",
+            "Cafe Coffee Day, Brigade Road",
+            "Cafe Pascucci",
+            "154 Breakfast Club",
+            "Blue Tokai Coffee Roasters, Koramangala",
+            "Art Cafe Kormangala"
+        ],
+        "park": [
+            "Cubbon Park",
+            "Lalbagh Botanical Garden",
+            "JP Park",
+            "Hebbal Lake Park",
+            "Bugle Rock Park",
+            "Bannerghatta Biological Park",
+            "M N Krishna Rao Park",
+            "Freedom Park",
+            "Indira Gandhi Musical Fountain Park",
+            "Jayaprakash Narayan Biodiversity Park"
+        ]
+    },
+    "Pune": {
+        "cafe": [
+            "Vohuman Cafe",
+            "Cafe Goodluck",
+            "Pagdandi Books Chai Cafe",
+            "One O Eight Cafe",
+            "Cafe Peter",
+            "Coffee Jar",
+            "The French Window Patisserie",
+            "Cafe Paashh",
+            "Fat Cat’s Cafe",
+            "Le Plaisir"
+        ],
+        "park": [
+            "Osho Teerth Park",
+            "Empress Botanical Garden",
+            "P L Deshpande Garden",
+            "Bund Garden",
+            "Okayama Friendship Garden",
+            "Saras Baug",
+            "Taljai Forest Park",
+            "Vetal Hill",
+            "Kamala Nehru Park",
+            "Joshi's Museum of Miniature Railways Garden"
+        ]
+    },
+    "Hyderabad": {
+        "cafe": [
+            "Autumn Leaf Cafe",
+            "The Roastery Coffee House",
+            "Beyond Coffee",
+            "Cafe Bahar",
+            "Cafe Graffiti",
+            "Coffee Cup",
+            "Cafe Niloufer",
+            "Conçu Jubilee Hills",
+            "Driven Cafe",
+            "Karachi Bakery Cafe"
+        ],
+        "park": [
+            "KBR National Park",
+            "NTR Gardens",
+            "Lumbini Park",
+            "Indira Park",
+            "Botanical Garden Kondapur",
+            "Sanjeevaiah Park",
+            "Mahavir Harina Vanasthali National Park",
+            "Durgam Cheruvu Park",
+            "Mrugavani National Park",
+            "Public Gardens"
+        ]
+    },
+    "Aurangabad": {
+        "cafe": [
+            "Cafe Coffee Day CIDCO",
+            "Brownie Point Cafe",
+            "Cafe Bollywood",
+            "Café De Pho",
+            "Cafe Friends",
+            "Cafe Kahwa",
+            "Cafe Espresso",
+            "Brewberrys Cafe",
+            "Cafe Chokolade",
+            "Beans & Brews Cafe"
+        ],
+        "park": [
+            "Siddharth Garden and Zoo",
+            "N-4 Garden",
+            "Himayat Bagh",
+            "Salim Ali Lake & Bird Sanctuary",
+            "Ganesh Garden",
+            "Joggers Park CIDCO",
+            "Dr. Babasaheb Ambedkar Garden",
+            "MGM Garden",
+            "Nandanvan Park",
+            "Kanchanwadi Garden"
+        ]
+    },
+    "Chennai": {
+        "cafe": [
+            "Amethyst Cafe",
+            "Writer's Cafe",
+            "Chamiers Cafe",
+            "Cafe Coffee Day Besant Nagar",
+            "Cafe Arabica",
+            "Ashvita Bistro",
+            "Wild Garden Cafe",
+            "Cafe Central Perk",
+            "The Brew Room",
+            "Cafe Cakewalk"
+        ],
+        "park": [
+            "Semmozhi Poonga",
+            "Guindy National Park",
+            "Anna Nagar Tower Park",
+            "Chetpet Eco Park",
+            "Tholkappia Poonga",
+            "Sivan Park",
+            "Natesan Park",
+            "Dr MGR Memorial Park",
+            "Jeeva Park",
+            "Children's Park Guindy"
+        ]
+    }
+}
